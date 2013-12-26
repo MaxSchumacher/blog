dff6141d35275530942a6d93efd9b4c4bd68d59c
iversity admin <iversityadmin@tims-MacBook-Pro.local>
2013-12-26 03:49:58 +0100

    The repository was initialized

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '1cc939744e35d2c3bfb855eac51edc8294448a487bae38dd16356bfd635309c8e8acabb36f9ad9f4a74e1fcc89ddf0f5ac623803154b4b3ef5d3080072fdbf07'
